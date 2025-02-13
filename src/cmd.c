24029a086d931efecb102635ae09416b6e33967d
Nguyen Chu Hoang Anh <nguyenc@im2ag-216-20.u-ga.fr>
2025-02-13 12:57:35 +0100

    Add skeleton and functions for pipe

--- src/cmd.c
+++ src/cmd.c
@@ -3,8 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int nb_commande(struct cmdline *l) {
-    return 1;
+static int nb_cmd(struct cmdline *l) {
+    int i = 0;
+    while (l->seq[i] != NULL) {
+        i++;
+    }
+    return i;
 }
 
 int cd(char *path) {
@@ -29,9 +33,45 @@ int internal(char **cmd) {
     return 0;
 }
 
+static int **create_pipes(int n_cmd) {
+    int **pipes = (int **)malloc(sizeof(int *) * n_cmd - 1);
+    if (pipes == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    pipes[0] = (int *)malloc(sizeof(int) * 2);
+    if (pipes[0] == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(pipes[0]) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    return pipes;
+}
+
+static void connect_pipes(int **pipes) {
+
+}
+
+static void redirection_in_out(struct cmdline *l) {
+    
+}
+
 void execution(struct cmdline *l) {
-    int nb_cmd = nb_commande(l);
-    for (int i = 0; i < nb_cmd; i++) {
+    if (l->seq[0] == NULL) {
+        return;
+    }
+
+    int n_cmd = nb_cmd(l);
+    int **pipes;
+
+    if (n_cmd > 1) {
+        pipes = create_pipes(n_cmd);
+    }
+
+    for (int i = 0; i < n_cmd; i++) {
         if (internal(l->seq[i])) {
             //
         } else {
@@ -41,18 +81,10 @@ void execution(struct cmdline *l) {
             if (pid == 0) {
                 // Fils
                 // dup2()
-                if (l->in != NULL) {
-                    int in = open(l->in, O_RDONLY);
-                    dup2(in, STDIN_FILENO);
+                if (execvp(l->seq[i][0], l->seq[0]) == -1) {
+                    fprintf(stderr, "%s : Command not found\n", l->seq[i][0]);
+                    exit(3);
                 }
-
-                if (l->out != NULL) {
-                    int out = open(l->out, O_CREAT | O_WRONLY, 0644);
-                    dup2(out, STDOUT_FILENO);
-                }
-
-                execvp(l->seq[0][0], l->seq[0]);
-                exit(0);
             } else if (pid > 0) {
                 pid = waitpid(pid, NULL, 0);
             } else {
